78123eebdf1ab335014b228fbe44d9d997541d72
Jakob Ruhe <jakob.ruhe@gmail.com>
2025-02-14 23:58:53 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,47 @@
+# Activate a nRF Connect toolchain environment
+
+This script activates a [nRF Connect SDK](https://github.com/nrfconnect/sdk-nrf)
+toolchain environment.
+
+It was written by Jakob Ruhe late 2024. Feel free to say something if you find
+the script useful or if you have any suggestions for improvements!
+
+## Installation
+
+Clone this repository and put it somewhere. In this documentation it is assumed
+that you clone it into `~/nrfenv` but you can put it wherever you want.
+
+## Usage
+
+Use the
+[nrfutil](https://www.nordicsemi.com/Products/Development-tools/nRF-Util) like
+this to get the paths to the installed toolchains:
+
+``` shell
+nrfutil toolchain-manager list
+```
+
+Example output:
+
+```
+  Version  Toolchain
+* v2.9.0   /home/jakob/ncs/toolchains/b77d8c1312
+```
+
+Now that you have the path to the toolchain you want to activate, you may verify
+that there is a file named `environment.json` in that directory.
+
+With this knowledge you can activate the toolchain environment by using this utility:
+
+``` shell
+~/nrfenv/nrfenv.py /home/jakob/ncs/toolchains/b77d8c1312/environment.json
+```
+
+To deactivate the environment, simply run:
+
+``` shell
+exit
+```
+
+## License
+See the [LICENSE](LICENSE) file for license rights and limitations (MIT).
---
+++ nrfenv.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import logging
+import subprocess
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Activates a nRF Connect toolchain environment",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "envfile", help="The environment file to use (typicaly named environment.json)"
+    )
+    parser.add_argument(
+        "--verbose", "-v", action="store_true", help="Enable verbose logging"
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG if args.verbose else logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+    )
+    for level in (logging.DEBUG, logging.INFO):
+        logging.addLevelName(level, logging.getLevelName(level).lower())
+
+    with open(args.envfile) as f:
+        env_mods = json.load(f)
+    newenv = os.environ.copy()
+    base = os.path.dirname(os.path.abspath(args.envfile))
+    for env_mod in env_mods["env_vars"]:
+        key = env_mod["key"]
+        values = env_mod.get("values", [env_mod.get("value", [])])
+        type = env_mod.get("type")
+        if type == "relative_paths":
+            values = [os.path.join(base, v) for v in values]
+        elif type == "string":
+            pass
+        else:
+            raise ValueError(f"Unsupported for key '{key}': '{type}'")
+        evt = env_mod.get("existing_value_treatment", "overwrite")
+        if evt == "prepend_to":
+            existing = newenv.get(key, "")
+            logging.debug(f"Prepending key '{key}' with '{':'.join(values)}'")
+            newenv[key] = ":".join(values + existing.split(":"))
+        elif evt == "overwrite":
+            logging.debug(f"Replacing key '{key}' with '{':'.join(values)}'")
+            newenv[key] = ":".join(values)
+        else:
+            raise ValueError(
+                f"Unsupported 'existing_value_treatment' for key '{key}': '{evt}'"
+            )
+
+    sh = os.getenv("SHELL", "sh")
+    print(
+        f"Running shell '{sh}' with new environment. Type 'exit' to return to the previous environment."
+    )
+    subprocess.run([sh], env=newenv)
+
+
+if __name__ == "__main__":
+    main()
